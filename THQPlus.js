142f48976ee20cf5d8753bf1d16c787a01f93abd
earlofcloves <133715833+earlofcloves@users.noreply.github.com>
2023-06-07 19:52:15 -0400

    Update THQPlus.js

--- THQPlus.js
+++ THQPlus.js
@@ -120,6 +120,8 @@ async function THQPlus() {
    async function updateTHQi() {
       if (debug) {console.log("update invasions")}      
       const invs=document.getElementsByClassName("info-card__content");
+      const i2=document.querySelectorAll(".info-card__content");
+      console.log(i2); return;
       if (debug) {console.log(Array.from(invs)[0]);return;console.log(invs.item(0))}    
       Array.from(invs).forEach(i=>{
          const invtype = i.childNodes[0]; 
