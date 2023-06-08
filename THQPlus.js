e57ecf0852933ce6b98b814c59a039b0c1f40337
earlofcloves <133715833+earlofcloves@users.noreply.github.com>
2023-06-07 20:58:03 -0400

    Update THQPlus.js

--- THQPlus.js
+++ THQPlus.js
@@ -26,38 +26,38 @@ async function THQPlus() {
                'Bossbot HQ': "Chip 'n Dale's"
             },
             cog: {
-               'Flunky':                  ["","B",1],
-               'Pencil Pusher':           ["Pencil Pusher","B",2],
-               'Yesman':                  ["*Yesman","B",3],
-               'Micro\x03manager':        ["MicroManager","B",4],
-               'Downsizer':               ["","B",5],
-               'Head Hunter':             ["","B",6],
-               'Corporate Raider':        ["Corp Raider","B",7],
-               'The Big Cheese':          ["Big Cheese","B",8],
-               'Bottom Feeder':           ["","B",1],
-               'Blood\u0003sucker':       ["Blood Sucker","B",2],
-               'Double Talker':           ["*Dbl Talker","B",3],
-               'Ambulance Chaser':        ["Amb Chaser","B",4],
-               'Back Stabber':            ["*Back Stabber","B",5],
-               'Spin Doctor':             ["*Spin Doctor","B",6],
-               'Legal Eagle':             ["","B",7],
-               'Big Wig':                 ["*Big Wig","B",8],
-               'Short Change':            ["","B",1],
-               'Penny Pincher':           ["","B",2],
-               'Tightwad':                ["","B",3],
-               'Bean Counter':            ["","B",4],
-               'Number Cruncher':         ["Nmbr Cruncher","B",5],
-               'Money Bags':              ["","B",6],
-               'Loan Shark':              ["","B",7],
-               'Robber Baron':            ["*Robber Baron","B",8],
-               'Cold Caller':             ["","B",1],
-               'Tele\u0003marketer':      ["Telecommuter","B",2],
-               'Name Dropper':            ["*Name Dropper","B",3],
-               'Glad Hander':             ["","B",4],
-               'Mover & Shaker':          ["*Mover&Shaker","B",5],
-               'Two-Face':                ["","B",6],
-               'The Mingler':             ["*Mingler","B",7],
-               'Mr. Hollywood':           ["*Mr Hollywood","B",8],
+               'FLUNKY':                  ["Flunky","B",1],
+               'PENCIL PUSHER':           ["Pencil Pusher","B",2],
+               'YESMAN':                  ["*Yesman","B",3],
+               'MICROMANAGER':        ["MicroManager","B",4],
+               'DOWNSIZER':               ["Downsizer","B",5],
+               'HEAD HUNTER':             ["Head Hunter","B",6],
+               'CORPORATE RAIDER':        ["Corp Raider","B",7],
+               'TEH BIG CHEESE':          ["Big Cheese","B",8],
+               'BOTTOM FEEDER':           ["Bottom Feeder","B",1],
+               'BLOODUSCKER':       ["Blood Sucker","B",2],
+               'DOUBLE TALKER':           ["*Dbl Talker","B",3],
+               'AMBULANCE CHASER':        ["Amb Chaser","B",4],
+               'BACK STABBER':            ["*Back Stabber","B",5],
+               'SPIN DOCTOR':             ["*Spin Doctor","B",6],
+               'LEGAL EAGLE':             ["Legal Eagle","B",7],
+               'BIG WIG':                 ["*Big Wig","B",8],
+               'SHORT CHANGE':            ["Short Change","B",1],
+               'PENNY PINCHER':           ["Penny Pincher","B",2],
+               'TIGHTWAD':                ["Tightwad","B",3],
+               'BEAN COUNTER':            ["Bean Counter","B",4],
+               'NUMBER CRUNCHER':         ["Number Cruncher","B",5],
+               'MONEY BAGS':              ["Money Bags","B",6],
+               'LOAN SHARK':              ["Loan Shark","B",7],
+               'ROBBER BARON':            ["*Robber Baron","B",8],
+               'COLD CALLER':             ["Cold Caller","B",1],
+               'TELEMARKETER':      ["Telecommuter","B",2],
+               'NAME DROPPER':            ["*Name Dropper","B",3],
+               'GLAD HANDER':             ["Glad Hander","B",4],
+               'MOVER & SHAKER':          ["*Mover&Shaker","B",5],
+               'TWO-FACE':                ["Two-Face","B",6],
+               'THE MINGLER':             ["*Mingler","B",7],
+               'MR. HOLLYWOOD':           ["*Mr Hollywood","B",8],
                'decode': {'B':'Boss','C':"Cash","L":"Law","S":"Sell"}
             },
          };
@@ -123,7 +123,7 @@ async function THQPlus() {
       console.log("invs",invs);
       Array.from(invs).forEach(i=>{
          const cog = i.childNodes[0], cogv=cog.innerText;
-         console.log(cogv, lookup.cog[cogv]);
+         console.log(cogv, invInfo(cogv));
          return;
          const street = g.childNodes[1]; streetv=street.innerText.split(" (")[0];
          const district = g.childNodes[2]; districtv=district.innerText.split("\n")[0];
@@ -154,21 +154,21 @@ async function THQPlus() {
          if (neighborhood(streetv)) {street.innerHTML=streetv+' <i>'+neighborhood(streetv)+'</i>'}
          if (inv[districtv] && invtypev.includes('BUILDING')) {
             //const [cogsKilled,cogsTotal] = inv[districtv].progress.split('/'); const cogsLeft=cogsTotal-cogsKilled; 
-            district.innerHTML=`${districtv}<br><b><i>${invInfo(inv[districtv].type)}</i></b>`;    
+            district.innerHTML=`${districtv}<br><b><i>Invasion ${invInfo(inv[districtv].type)}</i></b>`;    
          }
       });
    }  
    
    function invInfo(cog) {
-      const coginfo=lookup.cog[cog];
+      const coginfo=lookup.cog[decodeCogname(cog)];
       if (debug) {console.log(decodeCogname(cog))}
       if (coginfo) {
          let [dispname,type,lev]=coginfo;
          if (dispname=="") {dispname=cog}
          lev=`${lev}-${Number(lev)+4}`;
-         return `Invasion ${dispname} ${lookup.cog.decode[type]} L${lev}`;
+         return `${dispname} ${lookup.cog.decode[type]} L${lev}`;
       }
-      else {return `Cogtype ${cog} not found`}
+      else {return cog}
    }
    
    function neighborhood(street) {
