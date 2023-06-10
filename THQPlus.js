2c8b516f5f8fc40f30fb85f90c746dba85aefaba
earlofcloves <133715833+earlofcloves@users.noreply.github.com>
2023-06-10 18:14:34 -0400

    Update THQPlus.js

--- THQPlus.js
+++ THQPlus.js
@@ -144,7 +144,7 @@ async function THQPlus() {
          if (neighborhood(streetv)) {street.innerHTML=streetv+' <i>'+neighborhood(streetv)+'</i>'}
          if (inv[districtv] && invtypev.includes('BUILDING')) {
             let [cogname,cogtype,coglvl,avgattack] = getCogInfo(inv[districtv].type); 
-            district.innerHTML=`${districtv}<br><span style="color:#880000;"><span><center><b>${cogname} ${cogtype} Invasion</center><br>`+
+            district.innerHTML=`${districtv}<br><span style="color:#880000;"><span><center><b>${cogname} ${cogtype} Invasion</center>`+
                                `<center><i>** Level ${coglvl}, AvgAttack ${avgattack} **</center></i></b></span></span>`;   
             g.parentElement.style.height="130px";
          }
